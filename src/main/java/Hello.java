0e8b1b254980c40033b774af4749d2486340439b
Murat Guney <muratguney@msn.com>
2021-01-26 15:22:54 -0500

    Movie1 branch updated

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World, B21");
         System.out.println("B21 future SDET learning Git");
+        //Watching more movie
     }
 }
